a583f37b941ba3c57bb4e6002ec4eb5f4a07ea77
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-08 23:57:04 -0400

    added exercise 11

---
+++ exercise11/src/main/java/exercise11/Solution11.java
@@ -0,0 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Samontanez
+ */
+
+package exercise11;
+
+import java.util.Scanner;
+
+public class Solution11 {
+    /*
+    Print "How many euros are you exchanging? "
+    scanner takes the input from the user
+    'euro' stores the input
+
+    Print "What is the current exchange rate?"
+    scanner takes the input
+    'rate' stores the input
+
+    'dollars' = euro * rate
+    'roundOff' = rounds the 'dollar' amount to the nearest tenths.
+
+    Print "<euro> euros at an exchange rate of <rate> is <dollars> U.S. dollars.
+     */
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("How many euros are you exchanging? ");
+        String euroStr = scanner.nextLine();
+        int euro = Integer.parseInt(euroStr);
+
+        System.out.print("What is the current exchange rate? ");
+        Scanner r8 = new Scanner(System.in);
+        float rate = r8.nextFloat();
+
+        float dollars = euro * rate;
+        double roundOff = Math.round(dollars * 100.0)/100.0;
+
+        System.out.println(euro + " euros at an exchange rate of " + rate + " is\n" +
+                roundOff + " U.S. dollars.");
+
+    }
+}
