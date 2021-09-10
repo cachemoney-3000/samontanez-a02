eb2ea91bf720f5fc1270b51add14c0b1a03307de
joshuas029 <jsamontanez@knights.ucf.edu>
2021-09-10 17:15:53 -0400

    Added exercise15

---
+++ exercise15/src/main/java/exercise15/Solution15.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Samontanez
+ */
+
+package exercise15;
+
+import java.util.Scanner;
+
+public class Solution15 {
+    /*
+    Print "What is your password? "
+    scanner takes the input from the user
+    'password' string stores the input
+
+    string 'key' = stores the string "pa$$word"
+
+    if the password is equals to the key {
+        printf("Welcome")
+    }
+
+    else{
+    Print "I don't know you."
+    }
+     */
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("What is the password? ");
+        Scanner passStr = new Scanner(System.in);
+        String password = passStr.nextLine();
+
+        String key = "pa$$word";
+
+        if(password.equals(key)){
+            System.out.println("Welcome!");
+        }
+        else{
+            System.out.println("I don't know you.");
+        }
+    }
+}
